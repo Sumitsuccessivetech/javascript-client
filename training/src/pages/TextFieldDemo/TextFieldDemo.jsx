ed41a74299eb4e6aee15f5f81b84882e7f1ddcb9
Sumit Upadhyay <sumit.upadhyay@tr03.successivetech.com>
2020-12-14 21:18:19 +0530

    feat: Introduction to state

--- training/src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ training/src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import { TextField } from '../../components';
-
+import { Slider} from '../../components/Slider'
+import { banners} from '../../configs/constants'
 import { Div } from '../../components/TextField/style';
 
 const TextFieldDemo = () => (
   <Div>
+      <div>
+      <Slider alt="No Image" duration="1000" height="300" random banner={banners} />
+    </div>
     <p><b>This is a Disabled Input</b></p>
     <TextField disabled="This is disabled" value="disabled input" />
     <p><b> A Valid Input</b></p>
