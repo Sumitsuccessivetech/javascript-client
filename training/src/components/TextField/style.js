8c08ff014b2cc30bce1ef6261567f9db1afc6195
Sumit Upadhyay <sumit.upadhyay@tr03.successivetech.com>
2020-12-15 13:44:35 +0530

    Feat: React- Form Validation

--- training/src/components/TextField/style.js
+++ training/src/components/TextField/style.js
@@ -1,30 +1,28 @@
 import styled, { css } from 'styled-components';
 
-const Div = styled.div `
-margin : 2px;
-border:1px solid black;
-box-shadow: 5px 10px 18px rgb(214, 212, 212);
+const Div = styled.div`
+margin: 2%;
 `;
-const Error = styled.p `
+const Error = styled.p`
 color: red;
 `;
-const Input = styled.input `
-width: 98.2%;
-padding: 0.8%;
+const Input = styled.input`
+width: 100%;
+padding: 0.7%;
 border: 1px solid gray;
 border-radius: 5px;
 color: solid gray;
 ${(props) => props.error
-&& css`
-border: 1px solid red;
+        && css`
+border: 1px solid black;
 color: red;
 `};
 }
 ${(props) => (props.value && !props.disabled && !props.error)
-&& css`
-border:1px solid orange;
+        && css`
+border: 1px solid orange;
 color: black;
 `};
 }
 `;
-export { Div, Error, Input  };
+export { Div, Error, Input };
