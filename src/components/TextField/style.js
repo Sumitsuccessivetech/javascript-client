657edda2e6664753341e59ea6e5bfac8634edce6
Sumit Upadhyay <sumit.upadhyay@tr03.successivetech.com>
2020-12-21 16:50:47 +0530

    Feat: Changes Donegit pull origin development

--- src/components/TextField/style.js
+++ src/components/TextField/style.js
@@ -1,30 +1,30 @@
 import styled, { css } from 'styled-components';
 
-const Div = styled.div `
+const Div = styled.div`
 margin : 2px;
 border:1px solid black;
 box-shadow: 5px 10px 18px rgb(214, 212, 212);
+padding:1px;
 `;
-const Error = styled.p `
+const Error = styled.p`
 color: red;
 `;
-const Input = styled.input `
+const Input = styled.input`
 width: 98.2%;
 padding: 0.8%;
 border: 1px solid gray;
 border-radius: 5px;
-color: solid gray;
 ${(props) => props.error
-&& css`
+        && css`
 border: 1px solid red;
 color: red;
 `};
 }
 ${(props) => (props.value && !props.disabled && !props.error)
-&& css`
+        && css`
 border:1px solid orange;
 color: black;
 `};
 }
 `;
-export { Div, Error, Input  };
+export { Div, Error, Input };
