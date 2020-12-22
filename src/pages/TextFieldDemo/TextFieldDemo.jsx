8326739eb8c693bfce98a33432ada6e8f49fd1cc
Sumit Upadhyay <sumit.upadhyay@tr03.successivetech.com>
2020-12-22 18:37:17 +0530

    Feat: Changes Donegit status

--- src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -7,7 +7,7 @@ import { Div } from '../../components/TextField/style';
 
 const TextFieldDemo = () => (
   <Div>
-      <div>
+    <div>
       <Slider alt="No Image" duration="1000" height="300" random banner={banners} />
     </div>
     <p><b>This is a Disabled Input</b></p>
