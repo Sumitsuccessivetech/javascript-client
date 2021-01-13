8599adc115589d9859f9dceea41b95df6ff350aa
Sumit Upadhyay <sumit.upadhyay@tr03.successivetech.com>
2021-01-13 12:14:06 +0530

    Feat: material UI- Styling, Dilaog and Form Validation

--- src/pages/Trainee/Trainee.jsx
+++ src/pages/Trainee/Trainee.jsx
@@ -3,40 +3,40 @@ import Button from '@material-ui/core/Button';
 import AddDialog from './components/AddDialog/AddDialog';
 
 export default class Trainee extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
 
     handleClickOpen = () => {
-        this.setState({ open: true });
+      this.setState({ open: true });
     };
 
     handleClose = () => {
-        const { open } = this.state;
-        this.setState({ open: false });
-        return open;
+      const { open } = this.state;
+      this.setState({ open: false });
+      return open;
     };
 
     handleSubmit = (data) => {
-        this.setState({
-            open: false,
-        }, () => {
-            console.log(data);
-        });
+      this.setState({
+        open: false,
+      }, () => {
+        console.log(data);
+      });
     }
 
     render() {
-        const { open } = this.state;
-        return (
-            <>
-            <NavBar />
-            <br />
-                <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>ADD TRAINEE</Button>
-                <AddDialog open={open} onClose={this.handleClose} onSubmit={() => this.handleSubmit} />
-            </>
-        );
+      const { open } = this.state;
+      return (
+        <>
+          <NavBar />
+          <br />
+          <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>ADD TRAINEE</Button>
+          <AddDialog open={open} onClose={this.handleClose} onSubmit={() => this.handleSubmit} />
+        </>
+      );
     }
 }
