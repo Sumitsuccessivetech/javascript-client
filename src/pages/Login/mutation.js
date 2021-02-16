aad6ca5ceda39525c7c7b8b73d4d7d68046ddeef
Sumit Upadhyay <sumit.upadhyay@tr03.successivetech.com>
2021-02-16 12:33:42 +0530

    Feat: Changes in Integretion of client!

--- src/pages/Login/mutation.js
+++ src/pages/Login/mutation.js
@@ -2,11 +2,7 @@ import { gql } from 'apollo-boost';
 
 const LOGIN_USER = gql`
 mutation loginUser($email: String!, $password: String!) {
-  loginUser(payLoad: { email: $email, password: $password}){
-    message
-    status
-    data
-  }
+  loginUser(payLoad: { email: $email, password: $password})
   }
 `;
 
